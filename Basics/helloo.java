5a995bd845494e5bccb556312c1ad8c68523f966
Rutuja Bobade <155938957+Rutuja-Bobade@users.noreply.github.com>
2024-06-24 10:53:26 +0530

    Create helloo.java

---
+++ Basics/helloo.java
@@ -0,0 +1,8 @@
+public class helloo {
+    public static void hello(){
+        System.out.println("Hello Everyone");
+    }
+    public static void main(String args[]) {
+        hello();
+    }
+}
